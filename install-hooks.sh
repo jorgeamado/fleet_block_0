77498bff7f300933e9dcb8586bac447cdfea5a02
george.l <georeg.lemeshko@gmail.com>
2023-07-09 18:05:12 +0200

    Add unity-git-hooks

---
+++ install-hooks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if ! [ -d ".git" ]; then
+    echo "Not in a git repository" >&2
+    exit 1
+fi
+
+SCRIPT_DIR=$(cd "${0%/*}" && pwd)
+
+SCRIPTS=(pre-commit post-checkout post-merge)
+
+for SCRIPT in "${SCRIPTS[@]}"; do
+#    ln -snf "$SCRIPT_DIR/$SCRIPT" ".git/hooks/$SCRIPT"
+	
+cat "$SCRIPT_DIR/$SCRIPT" >> ".git/hooks/$SCRIPT"
+done
+
+echo "Installed ${SCRIPTS[*]}"
